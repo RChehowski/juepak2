d723c15b443cb9060b28a58a4567ed6fbf683c6b
Roman Chehowski <r.chehowsky@gmail.com>
2019-01-10 11:12:02 +0200

    findCommonPath now takes roots into account

--- src/main/java/eu/chakhouski/juepak/util/PathUtils.java
+++ src/main/java/eu/chakhouski/juepak/util/PathUtils.java
@@ -33,64 +33,80 @@ public class PathUtils
 
     public static Path findCommonPath(boolean absolutize, Path... paths)
     {
-        Path commonPath = null;
+        // Quit if no paths to compare
+        if ((paths == null) || (paths.length == 0))
+        {
+            return null;
+        }
 
         final int numPaths = paths.length;
-        if (numPaths > 0)
+
+        // Make absolute paths if necessary
+        for (int i = 0; absolutize && i < numPaths; i++)
         {
-            // Absolutize paths if necessary
-            for (int i = 0; absolutize && i < numPaths; i++)
+            final Path p = paths[i];
+            paths[i] = (p != null && !p.isAbsolute()) ? p.toAbsolutePath() : p;
+        }
+
+        // Find first non-null base path
+        Path basePath = null;
+        Path commonRoot = null;
+
+        int indexOfBasePath;
+        for (indexOfBasePath = 0; (basePath == null) && (indexOfBasePath < numPaths); indexOfBasePath++)
+        {
+            if (paths[indexOfBasePath] != null)
             {
-                final Path p = paths[i];
-                paths[i] = (p != null && !p.isAbsolute()) ? p.toAbsolutePath() : p;
+                basePath = paths[indexOfBasePath];
+                commonRoot = basePath.getRoot();
             }
+        }
 
-            // Find first non-null base path
-            Path basePath = null;
-            Path basePathRoot = null;
+        // Quit if no base path or no (initial) common root
+        if ((basePath == null) || (commonRoot == null))
+        {
+            return null;
+        }
 
-            int indexOfBasePath;
-            for (indexOfBasePath = 0; indexOfBasePath < numPaths && (basePath == null); indexOfBasePath++)
+        // Set optimistic max matches
+        int maxMatches = basePath.getNameCount();
+
+        // Iterate trough path finding minimal similarity
+        for (int i = indexOfBasePath + 1; (i < numPaths); i++)
+        {
+            final Path p = paths[i];
+            if ((p != null))
             {
-                final Path maybeBasePath = paths[indexOfBasePath];
-                if (maybeBasePath != null)
+                if (commonRoot.equals(p.getRoot()))
                 {
-                    basePath = maybeBasePath;
-                    basePathRoot = basePath.getRoot();
-                }
-            }
+                    final int minLength = Math.min(basePath.getNameCount(), p.getNameCount());
 
-            // Find matches
-            if (basePath != null)
-            {
-                int maxMatches = basePath.getNameCount();
-
-                // Iterate trough path finding minimal similarity
-                for (int i = indexOfBasePath + 1; i < numPaths; i++)
-                {
-                    final Path p = paths[i];
-                    if ((p != null) && Objects.equals(basePathRoot, p.getRoot()))
+                    // Common root is still the same, compare parts
+                    for (int j = 0; j < minLength; j++)
                     {
-                        commonPath = basePathRoot;
-
-                        for (int j = 0; j < Math.min(basePath.getNameCount(), p.getNameCount()); j++)
+                        if (!basePath.getName(j).equals(p.getName(j)))
                         {
-                            if (!basePath.getName(j).equals(p.getName(j)))
-                            {
-                                maxMatches = Math.min(maxMatches, j);
-                                break;
-                            }
+                            // Found minimum, compare and quit loop
+                            maxMatches = Math.min(maxMatches, j);
+                            break;
                         }
                     }
                 }
-
-                // Connect paths together
-                if (commonPath != null)
+                else
                 {
-                    for (int i = 0; i < maxMatches; i++)
-                        commonPath = commonPath.resolve(basePath.getName(i));
+                    // No more common root
+                    commonRoot = null;
+                    break;
                 }
             }
+        }
+
+        Path commonPath = commonRoot;
+
+        // Connect paths together
+        for (int i = 0; (commonPath != null) && (i < maxMatches); i++)
+        {
+            commonPath = commonPath.resolve(basePath.getName(i));
         }
 
         return commonPath;
